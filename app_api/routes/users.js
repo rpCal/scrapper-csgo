2a72c064a12531dcdcf33b3d36e940dcacd57e35
Robert Całka <calka.rob@gmail.com>
2018-03-06 13:26:09 +0100

    up

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  const MongoClient = require('mongodb').MongoClient;
+  const url = 'mongodb://user_write:Test1234@ds121088.mlab.com:21088/csgo';
+  const dbName = 'csgo';
+  const collection_name = 'rounds';
+  MongoClient.connect(url, function(err, client) {
+    if(err){ return next(err);}
+    var db = client.db(dbName);
+    db.collection(collection_name, function(err, collection) {;
+      if(err){ return next(err);}
+      var result = collection.find({
+        "id":{ 
+          "$gt": 1241201,  
+          // "$lt": 1253939,  // start of key users
+        },
+        // 'users.1': {$exists: true}
+      }).toArray(function(err, result){
+        if(err){ return next(err);}
+        res.json(result);
+        client.close();
+      })
+    })
+  });
 });
 
 module.exports = router;
