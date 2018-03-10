cd2f965fc9bb69e65e19a5940cc76476f57886b9
Robert Całka <calka.rob@gmail.com>
2018-03-10 12:30:06 +0100

    up

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
   const MongoClient = require('mongodb').MongoClient;
   const url = 'mongodb://user_write:Test1234@ds121088.mlab.com:21088/csgo';
   const dbName = 'csgo';
@@ -18,8 +17,66 @@ router.get('/', function(req, res, next) {
         .find({"id":
           { "$gt": 1241201 } 
           // { "$gt": 1255151 } 
+        },{
+          sort: { id: 1 },
+          limit: 50000,
+          fields: {
+            "_id": 0,
+            // "id":1,
+            // "t":1,
+            // "n":1,
+            // "c":1,
+            // "sum_g": 1,
+            // "sum_r": 1,
+            // "sum_b": 1,
+            "hash":0,
+            "salt":0,
+            "num":0,
+            "users": 0,
+          }
         })
-        .sort({ id: 1 })
+        .toArray(function(err, result){
+          if(err){ return next(err);}
+          res.json(result);
+          client.close();
+        })
+    })
+  });
+});
+
+router.get('/all', function(req, res, next) {
+  const MongoClient = require('mongodb').MongoClient;
+  const url = 'mongodb://user_write:Test1234@ds121088.mlab.com:21088/csgo';
+  const dbName = 'csgo';
+  const collection_name = 'rounds';
+  
+  MongoClient.connect(url, function(err, client) {
+    if(err){ return next(err);}
+    var db = client.db(dbName);
+    db.collection(collection_name, function(err, collection) {;
+      if(err){ return next(err);}
+      var result = collection
+        .find({"id":
+          { "$gt": 1241201 } 
+          // { "$gt": 1255151 } 
+        },{
+          sort: { id: 1 },
+          limit: 50000,
+          // fields: {
+            // "_id": 0,
+            // "id":1,
+            // "t":1,
+            // "n":1,
+            // "c":1,
+            // "sum_g": 1,
+            // "sum_r": 1,
+            // "sum_b": 1,
+            // "hash":0,
+            // "salt":0,
+            // "num":0,
+            // "users": 0,
+          // }
+        })
         .toArray(function(err, result){
           if(err){ return next(err);}
           res.json(result);
